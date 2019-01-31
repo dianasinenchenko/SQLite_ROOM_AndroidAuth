c23dc44ade4febca1cd1a430660e5606c18729dc
Diana <diana.sinenchenko@gmail.com>
2019-01-31 11:38:43 +0200

    create Database obj

--- app/src/main/java/com/devitis/sqlite_room_androidauth/MainActivity.java
+++ app/src/main/java/com/devitis/sqlite_room_androidauth/MainActivity.java
@@ -1,5 +1,6 @@
 package com.devitis.sqlite_room_androidauth;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // create Database obj
+        AppDatabase db_create =  Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database").build();
         // get DB
         AppDatabase db = App.getInstance().getDatabase();
         // from Database obj get Dao
