cff23636e46c6a699bf4a06a078a9fcdd3095d34
Diana <diana.sinenchenko@gmail.com>
2019-01-31 13:42:49 +0200

    initialize elements layout in UI

--- app/src/main/java/com/devitis/sqlite_room_androidauth/MainActivity.java
+++ app/src/main/java/com/devitis/sqlite_room_androidauth/MainActivity.java
@@ -3,20 +3,27 @@ package com.devitis.sqlite_room_androidauth;
 import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button all_employee_button;
+    EditText employee_editText;
+    MultiAutoCompleteTextView all_employee_multiAutoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // create Database obj
-        AppDatabase db_create =  Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "database").build();
-        // get DB
-        AppDatabase db = App.getInstance().getDatabase();
-        // from Database obj get Dao
-        EmployeeDao employeeDao = db.employeeDao();
+
+        all_employee_button = (Button) findViewById(R.id.all_employee_button);
+        employee_editText = (EditText) findViewById(R.id.employee_editText);
+        all_employee_multiAutoCompleteTextView = (MultiAutoCompleteTextView) findViewById(R.id.all_employee_multiAutoCompleteTextView);
+
+
+
+
 
     }
 }
