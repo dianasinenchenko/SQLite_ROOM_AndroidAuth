b12e7b1afc55a4c4ed155145cf342f2034c5b640
Diana <diana.sinenchenko@gmail.com>
2019-01-31 11:12:52 +0200

    create abstract class AppDatabase and create abst methods (@Database its main class for DB)

---
+++ app/src/main/java/AppDatabase.java
@@ -0,0 +1,11 @@
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by Diana on 31.01.2019.
+ */
+@Database(entities = {Employee.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract EmployeeDao employeeDao();
+
+}
