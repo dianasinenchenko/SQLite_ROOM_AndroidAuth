0e8b15eda03b7ac1fe84d8d8527179316cfc99c7
Diana <diana.sinenchenko@gmail.com>
2019-01-31 13:51:32 +0200

    add some employee to DB

--- app/src/main/java/com/devitis/sqlite_room_androidauth/App.java
+++ app/src/main/java/com/devitis/sqlite_room_androidauth/App.java
@@ -3,6 +3,8 @@ package com.devitis.sqlite_room_androidauth;
 import android.app.Application;
 import android.arch.persistence.room.Room;
 
+import java.util.List;
+
 /**
  * Created by Diana on 31.01.2019.
  */
@@ -18,6 +20,23 @@ public class App extends Application {
         instance = this;
         database = Room.databaseBuilder(this, AppDatabase.class, "database")
                 .build();
+
+        // get DB
+        AppDatabase db = App.getInstance().getDatabase();
+        // from Database obj get Dao
+        EmployeeDao employeeDao = db.employeeDao();
+
+        List<Employee> employees = employeeDao.getAll();
+
+
+        Employee employee = new Employee();
+        employee.id = 1;
+        employee.name = "John Smith";
+        employee.salary = 10000;
+
+        employeeDao.insert(employee);
+
+
     }
 
     public static App getInstance() {
